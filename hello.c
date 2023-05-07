53b93691eac21ad2a69f527f3cadd48c3fb90394
krogh <kroghlin@hotmail.com>
2023-05-07 10:04:14 +0800

    change 1

--- hello.c
+++ hello.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(void)
+int main(int argc, char [] argv)
 {
     printf("Hello, C !\n");
+	printf("argc = %d\n");
+	if (argc > 0) {
+		printf("argv[0] = %s\n", argv[0]);
+		printf("argv[1] = %s\n", argv[1]);
+	}
     return 0;
 }
 
