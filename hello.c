b58ed586a032044166dea00c78ed010862cfbbe0
kroghlin <kroghlin@gmail.com>
2023-05-08 14:15:54 +0800

    add time/localtime

--- hello.c
+++ hello.c
@@ -3,17 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 // #include <math.h>
 
 int main(int argc, char ** argv)
 {
+    struct tm tm1;
     printf("Hello, C !\n");
-	printf("argc = %d\n");
+	printf("argc = %d\n", argc);
 	if (argc > 0) {
 		printf("argv[0] = %s\n", argv[0]);
 		printf("argv[1] = %s\n", argv[1]);
 	}
     printf("abs(-35) = %d\n", abs(-35));
+
+    struct tm* ptr;
+    time_t t;
+    printf("time(), localtime() return localtime \n");
+    t = time(NULL);
+    ptr = localtime(&t);
+    printf("%s", asctime(ptr));
 
     return 0;
 }
