532ea1faf5739b0637a0046fad96f529ff38e7fb
kroghlin <kroghlin@gmail.com>
2023-05-09 00:18:17 +0800

    add timeDem()

--- hello.c
+++ hello.c
@@ -6,10 +6,20 @@
 #include <time.h>
 // #include <math.h>
 
+void timeDemo()
+{
+    struct tm* ptr;
+    time_t t;
+    printf("time(), localtime() return localtime \n");
+    t = time(NULL);
+    ptr = localtime(&t);
+    printf("%s", asctime(ptr));
+}
+
 int main(int argc, char ** argv)
 {
     struct tm tm1;
-    printf("Hello, C !\n");
+    printf("Hello, macOS C !\n");
 	printf("argc = %d\n", argc);
 	if (argc > 0) {
 		printf("argv[0] = %s\n", argv[0]);
@@ -17,12 +27,15 @@ int main(int argc, char ** argv)
 	}
     printf("abs(-35) = %d\n", abs(-35));
 
+    timeDemo();
+/*
     struct tm* ptr;
     time_t t;
     printf("time(), localtime() return localtime \n");
     t = time(NULL);
     ptr = localtime(&t);
     printf("%s", asctime(ptr));
+    */
 
     return 0;
 }
