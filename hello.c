24acdd2db8f0bcbacd5d8fb0090c8345d8962ef0
kroghlin <kroghlin@gmail.com>
2023-05-08 13:57:10 +0800

    add abs()

--- hello.c
+++ hello.c
@@ -1,9 +1,11 @@
 /* hello.c */
+/* 2023/05/08 clone from GitHub */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+// #include <math.h>
 
-int main(int argc, char [] argv)
+int main(int argc, char ** argv)
 {
     printf("Hello, C !\n");
 	printf("argc = %d\n");
@@ -11,6 +13,8 @@ int main(int argc, char [] argv)
 		printf("argv[0] = %s\n", argv[0]);
 		printf("argv[1] = %s\n", argv[1]);
 	}
+    printf("abs(-35) = %d\n", abs(-35));
+
     return 0;
 }
 
